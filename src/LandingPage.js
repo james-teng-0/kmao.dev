6a21b52197c4c8b14ddc5dad43efa3a423bb5686
James Teng <jamest.1402@gmail.com>
2022-07-25 00:53:01 +1000

    update

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -15,43 +15,22 @@ const LandingPage = () => {
   });
 
 
-  const [navbarOpen, setNavbarOpen] = useState(false)
-
-  const handleToggle = () => {
-    setNavbarOpen(!navbarOpen)
-  }
-
   return (
     <section className="landing-page-wrapper">
       <nav id="nav-bar">
-        <div id="nav-right">
-          {isDesktop ? (
-            <>
-              <a className="nav-item" href="about"><span className="highlight-primary">/</span>About</a>
-              <a className="nav-item" href="about"><span className="highlight-primary">/</span>Experience</a>
-              <a className="nav-item" href="about"><span className="highlight-primary">/</span>Work</a>
-              <a className="nav-item" href="about"><span className="highlight-primary">/</span>Contact</a>
-              <a className="button" id="resume-button" href="resume">Resume</a>
-            </>
-          ) : (
-            <>
-              <button id="hamburger-menu" onClick={handleToggle}><img alt="hamburger-menu" src="hamburger.png" width= "70px"></img></button>
-              <ul className={`menuNav ${navbarOpen ? "showMenu" : ""}`}></ul>
-            </>
-          )}
-        </div>
       </nav>
       <div id="title-wrapper">
         <h1 className="title-heading">
           Hey, I'm <span className="highlight-primary">Kevin Mao</span>. <br/>
-          <span className="title-heading-secondary">I code smart contracts.</span>
+          <span className="title-heading-secondary">I code smart contracts</span> <br/>
+          <span className="title-heading-secondary">& sculpt ideas.</span>
         </h1>
         <p className="title-intro">
-          I'm a second year CS/Finance student @ UNSW dedicated to building in crypto since 2017.<br/>
-          I'm also the president of <a href="http://uncb.xyz/" rel="noreferrer" target="_blank"><span  className="highlight-secondary">UNCB</span></a> and in my spare time, I play poker & figure skate.
+          I'm presently on program leave from my CS/Finance degree @ UNSW. I've been dedicated to building in crypto since 2017.<br/>
+          Currently, I work on <a href="http://r3cursive.io/" rel="noreferrer" target="_blank"><span  className="highlight-secondary">R3cursive</span></a> as a founder-contributor & <a href="https://www.augmintedlabs.io/" rel="noreferrer" target="_blank"><span className="highlight-secondary">Augminted Labs</span></a> as a Product Manager.
         </p>
         <p className="title-intro" style={{"margin-bottom": "80px"}}>
-          Currently, I'm a Software Engineer @ <a href="http://gmistudios.com.au/" rel="noreferrer" target="_blank"><span className="highlight-secondary">GMI Studios</span></a> building cool things...
+          Previously, I was president of <a href="http://uncb.xyz/" rel="noreferrer" target="_blank"><span className="highlight-secondary">UNCB</span></a> & a Software Engineer @ <a href="http://gmistudios.com.au/" rel="noreferrer" target="_blank"><span className="highlight-secondary">GMI Studios</span></a>. In my spare time, I train Muay Thai & card count. 
         </p>
         <a className="button" href="https://www.kmao.blog/" target="_blank" rel="noreferrer">Check out my blog!</a>
       </div>
@@ -66,9 +45,6 @@ const LandingPage = () => {
               <li><a href="https://www.linkedin.com/in/kmao/" rel="noreferrer" target="_blank"><img alt="linkedin" src="./linkedin.svg"></img></a></li>
             </ul>
           </div>
-          <p id="learn-more">
-            Learn More {'>>'}
-          </p>
         </>
       ) : (
         <></>
